3e728362cc7502c074a40115b9acf4a2313c430a
MotiBartov <motib@tikalk.com>
2018-02-20 14:05:49 +0200

    models

--- app/src/main/java/com/tikal/sinister/data/Category.kt
+++ app/src/main/java/com/tikal/sinister/data/Category.kt
@@ -3,6 +3,4 @@ package com.tikal.sinister.data
 /**
  * Created by ronelg on 20/02/2018.
  */
-class Category {
-
-}
+data class Category(val id: String?, val name: String, val solved: Boolean, val quiz: List<Quiz>, val scores: List<Int>, val theme: String)
--- app/src/main/java/com/tikal/sinister/data/Quiz.kt
+++ app/src/main/java/com/tikal/sinister/data/Quiz.kt
@@ -3,6 +3,4 @@ package com.tikal.sinister.data
 /**
  * Created by ronelg on 20/02/2018.
  */
-class Quiz {
-
-}
+data class Quiz (val id: String, val type: String, val options: List<String>, val answer: Int)
