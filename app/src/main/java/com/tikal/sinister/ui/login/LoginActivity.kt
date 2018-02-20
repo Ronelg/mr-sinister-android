c930551f796ec19b1ad969bf2452d3373c121399
Ronel Girat <r.girat@gmail.com>
2018-02-20 15:47:28 +0200

    login

---
+++ app/src/main/java/com/tikal/sinister/ui/login/LoginActivity.kt
@@ -0,0 +1,72 @@
+package com.tikal.sinister.ui.login
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import timber.log.Timber
+import java.util.*
+
+
+/**
+ * Created by ronelg on 20/02/2018.
+ */
+class LoginActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 123
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                AuthUI.IdpConfig.EmailBuilder().build(),
+                                AuthUI.IdpConfig.PhoneBuilder().build(),
+                                AuthUI.IdpConfig.GoogleBuilder().build()))
+                        .build(),
+                RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            // Successfully signed in
+            if (resultCode == Activity.RESULT_OK) {
+//                startActivity(SignedInActivity.createIntent(this, response))
+                finish()
+                return
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Timber.d("sign_in_cancelled")
+//                    showSnackbar(R.string.sign_in_cancelled)
+                    return
+                }
+
+                if (response.errorCode == ErrorCodes.NO_NETWORK) {
+                    Timber.d("no_internet_connection")
+//                    showSnackbar(R.string.no_internet_connection)
+                    return
+                }
+
+                if (response.errorCode == ErrorCodes.UNKNOWN_ERROR) {
+                    Timber.d("unknown_error")
+//                    showSnackbar(R.string.unknown_error)
+                    return
+                }
+            }
+
+            Timber.d("unknown_sign_in_response")
+//            showSnackbar(R.string.unknown_sign_in_response)
+        }
+    }
+}
